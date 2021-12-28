ed43bdc2bf028cbeecbf3802da977409529bf14c
Ali <aliesmaeeli20@gmail.com>
2021-12-28 15:16:10 +0330

    init commit

---
+++ src/quasar-user-options.js
@@ -0,0 +1,12 @@
+
+import './styles/quasar.scss'
+import lang from 'quasar/lang/fa-IR'
+import '@quasar/extras/material-icons/material-icons.css'
+
+// To be used on app.use(Quasar, { ... })
+export default {
+  config: {},
+  plugins: {
+  },
+  lang: lang
+}
