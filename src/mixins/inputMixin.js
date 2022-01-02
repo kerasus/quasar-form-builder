4b2d1b0ef0231fd1ecc7ba7a3d4015b635eb4cc6
mary_razmjoo <maryamrazmjoo76@yahoo.com>
2022-01-02 09:56:16 +0330

    added dateTime input

--- src/mixins/inputMixin.js
+++ src/mixins/inputMixin.js
@@ -56,6 +56,10 @@ export default {
       default: false,
       type: Boolean
     },
+    date: {
+      default: false,
+      type: Boolean
+    },
     range: {
       default: false,
       type: Boolean
