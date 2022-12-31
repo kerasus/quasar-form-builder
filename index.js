e5a3a1b31a177cd8a1d89e0290b82d17e68f29ff
kerasus <aliesmaeeli20@gmail.com>
2022-12-31 15:08:44 +0330

    require default

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-'use strict'
-
-const FormBuilder = require('./src/FormBuilder')
-const inputMixin = require('./src/mixins/inputMixin')
-const FormBuilderGenerator = require('./src/components/formBuilderGenerator')
+const FormBuilder = require('./src/FormBuilder.vue').default
+const inputMixin = require('./src/mixins/inputMixin.js').default
+const FormBuilderGenerator = require('./src/components/formBuilderGenerator.vue').default
 const FormBuilderObject = {FormBuilder, inputMixin, FormBuilderGenerator}
 
-module.exports = FormBuilderObject
-module.exports.default = FormBuilderObject
+export {
+    FormBuilderObject as default,
+    FormBuilder,
+    inputMixin,
+    FormBuilderGenerator
+}
