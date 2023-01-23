1578e8e41b1796a5ffa00352a7c4c92a34b585c0
kerasus <aliesmaeeli20@gmail.com>
2023-01-23 11:42:26 +0330

    add submit btn

---
+++ src/components/FormBuilderSubmit.vue
@@ -0,0 +1,119 @@
+<template>
+  <q-btn :label="label"
+         :align="align"
+         :flat="flat"
+         :color="color"
+         :text-color="textColor"
+         :size="size"
+         :padding="padding"
+         :icon="icon"
+         :stack="stack"
+         :stretch="stretch"
+         :icon-right="iconRight"
+         :no-wrap="noWrap"
+         :unelevated="unelevated"
+         :dense="dense"
+         :rounded="rounded"
+         :push="push"
+         :square="square"
+         :glossy="glossy"
+         :outline="outline"
+         :loading="loading"
+         :disable="disable"
+         :percentage="percentage"
+         :class="customClass"
+         :input-class="customClass"
+         @click="onClick"
+         @keypress="onKeyPress"
+  />
+</template>
+
+<script>
+import inputMixin from '../mixins/inputMixin.js'
+export default {
+  name: 'FormBuilderSubmit',
+  mixins: [inputMixin],
+  props: {
+    name: {
+      default: '',
+      type: String,
+    },
+    size: {
+      default: '',
+      type: String,
+    },
+    padding: {
+      default: '',
+      type: String,
+    },
+    color: {
+      default: 'primary',
+      type: String,
+    },
+    textColor: {
+      default: 'primary',
+      type: String,
+    },
+    icon: {
+      default: '',
+      type: String,
+    },
+    iconRight: {
+      default: '',
+      type: String,
+    },
+    align: {
+      default: undefined,
+      type: ['left' | 'right' | 'center' | 'around' | 'between' | 'evenly' | undefined],
+    },
+    dense: {
+      default: false,
+      type: Boolean,
+    },
+    outline: {
+      default: false,
+      type: Boolean,
+    },
+    flat: {
+      default: false,
+      type: Boolean,
+    },
+    unelevated: {
+      default: false,
+      type: Boolean,
+    },
+    rounded: {
+      default: false,
+      type: Boolean,
+    },
+    push: {
+      default: false,
+      type: Boolean,
+    },
+    square: {
+      default: false,
+      type: Boolean,
+    },
+    glossy: {
+      default: false,
+      type: Boolean,
+    },
+    noWrap: {
+      default: false,
+      type: Boolean,
+    },
+    stack: {
+      default: false,
+      type: Boolean,
+    },
+    stretch: {
+      default: false,
+      type: Boolean,
+    },
+    percentage: {
+      default: 0,
+      type: Number,
+    },
+  },
+};
+</script>
