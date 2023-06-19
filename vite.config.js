97d8d7419974701115ffcf5d607ea13d15dff2e1
JasGH <yasaman.gonehi@gmail.com>
2023-06-19 17:27:56 +0330

    config for bundler added

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,30 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 const path = require('path')
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, './index.js'),
+      name: 'quasarFormBuilder',
+      // the proper extensions will be added
+      fileName: 'quasar-form-builder'
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
   plugins: [vue()],
   resolve: {
     alias: {
