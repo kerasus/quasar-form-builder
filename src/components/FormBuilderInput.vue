7df3136ff2570db434eb1f582841a14e30bca53b
ali <a.esmaeili@asredanesh.com>
2025-01-27 11:08:50 +0330

    update input props

--- src/components/FormBuilderInput.vue
+++ src/components/FormBuilderInput.vue
@@ -43,6 +43,7 @@ import { useInputComposable } from '@/composables/useInputComposable'
 
 // Define the extended type with additional properties.
 export type FormBuilderInputType = FormBuilderGenericInputType & {
+  value: string | number | null;
   autogrow: boolean;
   mask?: string;
   fillMask?: string;
@@ -58,84 +59,52 @@ export type FormBuilderInputType = FormBuilderGenericInputType & {
   rounded?: boolean;
   outlined?: boolean;
 }
+
+const FormBuilderInputDefaults: FormBuilderInputType = {
+  value: null,
+  autogrow: false,
+  mask: undefined,
+  fillMask: undefined,
+  reverseFillMask: undefined,
+  inputType: 'text',
+  maxlength: undefined,
+  hint: undefined,
+  placeholder: '',
+  error: false,
+  errorMessage: '',
+  filled: false,
+  lazyRules: false,
+  rounded: false,
+  outlined: false
+}
+
 const props = withDefaults(
   defineProps<FormBuilderInputType>(),
   {
-    autogrow: false
+    ...FormBuilderGenericInputDefaults,
+    ...FormBuilderInputDefaults
   }
 )
-
-// // Define the props separately from the type definition
-// const props = defineProps({
-//   // Spread the simple defaults from FormBuilderGenericInputDefaults
-//   ...FormBuilderGenericInputDefaults,
-//
-//   // Handle more complex properties explicitly
-//   autogrow: { type: Boolean, default: false },
-//   mask: { type: String, default: undefined },
-//   fillMask: { type: String, default: undefined },
-//   reverseFillMask: { type: Boolean, default: undefined },
-//   inputType: { type: String, default: 'text' },
-//   maxlength: { type: [String, Number], default: undefined },
-//   hint: { type: String, default: undefined },
-//   placeholder: { type: String, default: '' },
-//   error: { type: Boolean, default: false },
-//   errorMessage: { type: String, default: '' },
-//   filled: { type: Boolean, default: false },
-//   lazyRules: { type: Boolean, default: false },
-//   rounded: { type: Boolean, default: false },
-//   outlined: { type: Boolean, default: false },
-//
-//   // Handle properties like `rules` and `type` with their correct types:
-//   rules: { type: [Array, String], default: [] },
-//   type: { type: [String, Object], default: 'input' } // Assuming type can be string or a component
-// })
-
-//
-// const props = defineProps({
-//   value: { type: [String, Number, Boolean], default: '' },
-//   autogrow: { type: Boolean, default: false },
-//   mask: { type: String, default: undefined },
-//   fillMask: { type: String, default: undefined },
-//   reverseFillMask: { type: Boolean, default: undefined },
-//   inputType: {
-//     type: String,
-//     default: 'text',
-//     validator: (value: string) => [
-//       'text', 'password', 'textarea', 'email', 'search', 'tel', 'file', 'number', 'url', 'time', 'date', 'datetime-local'
-//     ].includes(value)
-//   },
-//   maxlength: { type: [String, Number], default: undefined },
-//   hint: { type: String, default: undefined },
-//   placeholder: { type: String, default: '' },
-//   error: { type: Boolean, default: false },
-//   errorMessage: { type: String, default: '' },
-//   filled: { type: Boolean, default: false },
-//   lazyRules: { type: Boolean, default: false },
-//   rounded: { type: Boolean, default: false },
-//   outlined: { type: Boolean, default: false }
-// })
 
 const emit = defineEmits(['update:value', 'input', 'change', 'onClick', 'onKeyPress'])
 
 const inputData = ref(props.value)
 
-// Use the composable for shared logic
-const { customClass, getValues, formHasFileInput, isFile } = useInputComposable(props)
+const { customClass } = useInputComposable(props)
 
 watch(() => props.value, (newValue) => {
   inputData.value = newValue
 })
 
-const change = (val: string | number | boolean) => {
+function change (val: string | number | boolean) {
   emit('update:value', val)
 }
 
-const onClick = (data: Event) => {
+function onClick(data: Event) {
   emit('onClick', data)
 }
 
-const onKeyPress = (data: Event) => {
+function onKeyPress(data: Event) {
   emit('onKeyPress', data)
 }
 
